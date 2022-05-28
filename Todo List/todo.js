8f6f8389aab713c2360e1ab2b0528e2625ce4a9f
AntonPodlatov <anton4792@tutanota.com>
2022-05-28 20:43:38 +0600

    Todo js 28.05

--- Todo List/todo.js
+++ Todo List/todo.js
@@ -1,70 +1,73 @@
-var button = document.getElementById("button");
+document.addEventListener("DOMContentLoaded", function () {
 
-button.addEventListener("click", function () {
-    var textFieldContent = document.getElementById("text-field").value;
+    var button = document.getElementById("button");
 
-    if (textFieldContent !== "") {
-        var list = document.getElementById("list");
-        var item = document.createElement("li");
-        item.setAttribute("class", "item")
+    button.addEventListener("click", function () {
+        var textFieldContent = document.getElementById("text-field").value;
 
-        var itemTextNode = document.createTextNode(textFieldContent);
-        var removeButton = getRemoveButton(item);
-        var editButton = getEditButton(itemTextNode);
+        if (textFieldContent !== "") {
+            var list = document.getElementById("list");
+            var item = document.createElement("li");
+            item.setAttribute("class", "item")
 
-        item.appendChild(itemTextNode);
-        item.appendChild(editButton);
-        item.appendChild(removeButton);
-        list.appendChild(item);
+            var itemTextNode = document.createTextNode(textFieldContent);
+            var removeButton = getRemoveButton(item);
+            var editButton = getEditButton(itemTextNode);
 
-        document.getElementById("text-field").value = "";
-    } else {
-        alert("Input something.");
+            item.appendChild(itemTextNode);
+            item.appendChild(editButton);
+            item.appendChild(removeButton);
+            list.appendChild(item);
+
+            document.getElementById("text-field").value = "";
+        } else {
+            alert("Input something.");
+        }
+    });
+
+    function getRemoveButton(item) {
+        var removeButton = document.createElement("input")
+        removeButton.setAttribute("type", "button");
+        removeButton.setAttribute("value", "remove item");
+
+        removeButton.addEventListener("click", function () {
+            item.parentNode.removeChild(item);
+        });
+
+        return removeButton;
+    }
+
+    function getEditButton(itemTextNode) {
+        var editButton = document.createElement("input");
+        editButton.setAttribute("type", "button");
+        editButton.setAttribute("value", "edit");
+
+        var editField = document.createElement("input");
+        editField.setAttribute("type", "text");
+        editField.setAttribute("value", itemTextNode.textContent);
+
+        editButton.addEventListener("click", function () {
+            itemTextNode.replaceWith(editField);
+
+            var saveButton = getSaveButton(editField);
+            editButton.replaceWith(saveButton);
+        });
+
+        return editButton;
+    }
+
+    function getSaveButton(editField) {
+        var saveButton = document.createElement("input");
+        saveButton.setAttribute("type", "button");
+        saveButton.setAttribute("value", "save");
+        saveButton.addEventListener("click", function () {
+
+            var newTextNode = document.createTextNode(editField.value);
+            editField.replaceWith(newTextNode);
+
+            saveButton.replaceWith(getEditButton(newTextNode))
+        });
+
+        return saveButton;
     }
 });
-
-function getRemoveButton(item) {
-    var removeButton = document.createElement("input")
-    removeButton.setAttribute("type", "button");
-    removeButton.setAttribute("value", "remove item");
-
-    removeButton.addEventListener("click", function () {
-        item.parentNode.removeChild(item);
-    });
-
-    return removeButton;
-}
-
-function getEditButton(itemTextNode) {
-    var editButton = document.createElement("input");
-    editButton.setAttribute("type", "button");
-    editButton.setAttribute("value", "edit");
-
-    var editField = document.createElement("input");
-    editField.setAttribute("type", "text");
-    editField.setAttribute("value", itemTextNode.textContent);
-
-    editButton.addEventListener("click", function () {
-        itemTextNode.replaceWith(editField);
-
-        var saveButton = getSaveButton(editField);
-        editButton.replaceWith(saveButton);
-    });
-
-    return editButton;
-}
-
-function getSaveButton(editField){
-    var saveButton = document.createElement("input");
-    saveButton.setAttribute("type", "button");
-    saveButton.setAttribute("value", "save");
-    saveButton.addEventListener("click", function () {
-
-        var newTextNode = document.createTextNode(editField.value);
-        editField.replaceWith(newTextNode);
-
-        saveButton.replaceWith(getEditButton(newTextNode))
-    });
-
-    return saveButton;
-}
