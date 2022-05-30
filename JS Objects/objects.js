af2a431bc7a04d501cf7ee444048b921777be35d
AntonPodlatov <anton4792@tutanota.com>
2022-05-30 22:50:53 +0600

    JS Objects 30.05

--- JS Objects/objects.js
+++ JS Objects/objects.js
@@ -1,65 +1,72 @@
-function getCountriesArray() {
-    return [
-        {
-            name: "Russia",
-            cities: [
-                {name: "Norilsk", population: 182701},
-                {name: "Moscow", population: 12635466},
-                {name: "Orel", population: 303696}
-            ]
-        },
-        {
-            name: "India",
-            cities: [
-                {name: "Bangalore", population: 8443675},
-                {name: "Mumbai", population: 15414288},
-                {name: "Kolkata", population: 4486679},
-                {name: "Jaipur", population: 3073350}
+(function () {
+    function getCountriesArray() {
+        return [
+            {
+                name: "Russia",
+                cities: [
+                    {name: "Norilsk", population: 182701},
+                    {name: "Moscow", population: 12635466},
+                    {name: "Orel", population: 303696}
+                ]
+            },
+            {
+                name: "India",
+                cities: [
+                    {name: "Bangalore", population: 8443675},
+                    {name: "Mumbai", population: 15414288},
+                    {name: "Kolkata", population: 4486679},
+                    {name: "Jaipur", population: 3073350}
+                ]
+            },
+            {
+                name: "Canada",
+                cities: [
+                    {name: "Toronto", population: 2503281},
+                    {name: "Montreal", population: 1942694},
+                    {name: "Quebec", population: 8294656}
+                ]
+            }
+        ];
+    }
 
-            ]
-        },
-        {
-            name: "Canada",
-            cities: [
-                {name: "Toronto", population: 2503281},
-                {name: "Montreal", population: 1942694},
-                {name: "Quebec", population: 8294656}
-            ]
-        }
-    ]
-}
+    function getMaxCitiesCount(array) {
+        var maxCitiesCount = 0;
 
-function getMaxCitiesCount(array) {
-    array.sort(function (e1, e2) {
-        return e1.cities.length - e2.cities.length;
-    })
+        array.forEach(function (currentCountry) {
+            var currentCountryCitiesCount = currentCountry.cities.length;
 
-    return array[array.length - 1].cities.length;
-}
+            if (maxCitiesCount < currentCountryCitiesCount) {
+                maxCitiesCount = currentCountryCitiesCount;
+            }
+        });
 
-function getCountriesWithMaxCitiesCount(countries) {
-    var maxCitiesCount = getMaxCitiesCount(countries)
+        return maxCitiesCount;
+    }
 
-    return countries.filter(function (countries) {
-        return countries.cities.length === maxCitiesCount;
-    });
-}
+    function getCountriesWithMaxCitiesCount(countries) {
+        var maxCitiesCount = getMaxCitiesCount(countries)
 
-function getTotalPopulationForCountries(countries) {
-    var countriesPopulation = {};
+        return countries.filter(function (countries) {
+            return countries.cities.length === maxCitiesCount;
+        });
+    }
 
-    countries.forEach(function (country) {
-        countriesPopulation[country.name] = getTotalPopulationFor1Country(country);
-    });
+    function getTotalPopulationForCountries(countries) {
+        var countriesPopulation = {};
 
-    return countriesPopulation;
-}
+        countries.forEach(function (country) {
+            countriesPopulation[country.name] = getTotalPopulationForCountry(country);
+        });
 
-function getTotalPopulationFor1Country(country) {
-    return country.cities.reduce(function (sum, city) {
-        return sum + city.population;
-    }, 0);
-}
+        return countriesPopulation;
+    }
 
-console.log(getCountriesWithMaxCitiesCount(getCountriesArray()))
-console.log(getTotalPopulationForCountries(getCountriesArray()))
+    function getTotalPopulationForCountry(country) {
+        return country.cities.reduce(function (sum, city) {
+            return sum + city.population;
+        }, 0);
+    }
+
+    console.log(getCountriesWithMaxCitiesCount(getCountriesArray()))
+    console.log(getTotalPopulationForCountries(getCountriesArray()))
+}());
