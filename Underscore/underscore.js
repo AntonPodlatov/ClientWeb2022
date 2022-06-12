08eeee519c0c18d8c511e64dfbfa33167cd5e095
AntonPodlatov <anton4792@tutanota.com>
2022-06-13 00:50:33 +0600

    Underscore 13.06

--- Underscore/underscore.js
+++ Underscore/underscore.js
@@ -1,48 +1,58 @@
-var humans = [
-    {age: 23, name: "Alex"},
-    {age: 28, name: "Ivan"},
-    {age: 30, name: "Anton"},
-    {age: 45, name: "Dmitry"},
-    {age: 44, name: "Boris"},
-    {age: 27, name: "Anna"},
-    {age: 19, name: "Nikolay"},
-    {age: 21, name: "Leonid"},
-    {age: 35, name: "Constantin"},
-    {age: 28, name: "Boris"},
-    {age: 27, name: "Boris"}
-];
+(function () {
+    var persons = [
+        {age: 23, name: "Alex"},
+        {age: 28, name: "Ivan"},
+        {age: 30, name: "Anton"},
+        {age: 45, name: "Dmitry"},
+        {age: 44, name: "Boris"},
+        {age: 27, name: "Anna"},
+        {age: 19, name: "Nikolay"},
+        {age: 21, name: "Leonid"},
+        {age: 35, name: "Constantin"},
+        {age: 28, name: "Boris"},
+        {age: 27, name: "Boris"}
+    ];
 
-var averageAge = _.reduce(humans, function (memo, human) {
-    return memo + human.age;
-}, 0) / humans.length;
+    var averageAge = _.reduce(persons, function (memo, person) {
+        return memo + person.age;
+    }, 0) / persons.length;
 
-var sortedHumansFrom20To30 = _.chain(humans)
-    .filter(function (human) {
-        return human.age >= 20 && human.age <= 30;
-    }).sortBy("age");
+    var sortedByAgePersonsFrom20To30 = _.chain(persons)
+        .filter(function (person) {
+            return person.age >= 20 && person.age <= 30;
+        })
+        .sortBy("age")
+        .value();
 
-var uniqueNamedHumansFrom30To20 = _.chain(humans)
-    .filter(function (human) {
-        return human.age >= 20 && human.age <= 30;
-    }).sortBy(function (human) {
-        return -human.age;
-    }).uniq(true, function (human) {
-        return human.name;
+    var uniqueNamesOfPersonsFrom30To20Sorted = _.chain(persons)
+        .filter(function (person) {
+            return person.age >= 20 && person.age <= 30;
+        })
+        .map(function (person) {
+            return person.name
+        }).uniq(true, function (name) {
+            return name;
+        }).sortBy(function (name) {
+            return name;
+        }).reverse().value();
+
+    var namesCounts = _.countBy(persons, "name");
+
+    console.log("average age: " + averageAge);
+    console.log("");
+
+    console.log("persons from 20 to 30 years old sorted by age:");
+    sortedByAgePersonsFrom20To30.forEach(function (person) {
+        console.log(person.name + " " + person.age);
     });
+    console.log("");
 
-var namesCount = _.countBy(humans, "name");
+    console.log("unique names of persons from 30 to 20 years old sorted by name:");
+    uniqueNamesOfPersonsFrom30To20Sorted.forEach(function (name) {
+        console.log(name);
+    });
+    console.log("");
 
-console.log("average age: " + averageAge);
-console.log("");
-
-console.log("humans from 20 to 30 years old sorted by age:");
-sortedHumansFrom20To30.forEach(e => console.log(e.name + " " + e.age));
-console.log("");
-
-console.log("unique named humans from 30 to 20 years old sorted by age:");
-uniqueNamedHumansFrom30To20.forEach(e => console.log(e.name + " " + e.age));
-console.log("");
-
-console.log("Names counts:");
-console.log(namesCount);
-
+    console.log("Names counts:");
+    console.log(namesCounts);
+}());
