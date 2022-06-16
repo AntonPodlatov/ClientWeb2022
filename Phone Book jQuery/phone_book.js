456230f5ebcf15db9d8f280265370b87bcf928f8
AntonPodlatov <anton4792@tutanota.com>
2022-06-16 20:51:07 +0600

    Phone Book jQuery 16.06

--- Phone Book jQuery/phone_book.js
+++ Phone Book jQuery/phone_book.js
@@ -13,19 +13,11 @@ $(document).ready(function () {
     phoneField.inputmask({regex: phoneRegexString});
 
     surnameField.on("input", function () {
-        if (!nameRegex.test(surnameField.val())) {
-            surnameField.addClass("is-invalid");
-        } else {
-            surnameField.removeClass("is-invalid");
-        }
+        surnameField.toggleClass("is-invalid", !nameRegex.test(surnameField.val()));
     });
 
     nameField.on("input", function () {
-        if (!nameRegex.test(nameField.val())) {
-            nameField.addClass("is-invalid");
-        } else {
-            nameField.removeClass("is-invalid");
-        }
+        nameField.toggleClass("is-invalid", !nameRegex.test(nameField.val()));
     });
 
     $("#button").click(function () {
@@ -34,7 +26,7 @@ $(document).ready(function () {
         if (!form[0].checkValidity()) {
             form.addClass('was-validated');
         } else {
-            if ($.inArray(phoneField.val(), getPhoneArray()) !== -1) {
+            if ($.inArray(phoneField.val(), getPhonesArray()) !== -1) {
                 alert("Контакт с таким номером уже есть в списке.");
                 return;
             }
@@ -53,18 +45,22 @@ $(document).ready(function () {
 
     function appendRow() {
         var newRow = $(
-            "<tr class='tbody_row align-middle text-center'><td></td><td>" + surnameField.val() +
-            "</td><td>" + nameField.val() +
-            "</td><td class='phone_table_detail'>+7-" + phoneField.val() +
-            "</td><td></td><td class='text-start'>" +
-            "<label><input type='checkbox' class='form-check-input tbody_checkbox'>" +
-            " Выбрать</label></td><tr>"
+            "<tr class='tbody_row align-middle text-center'>" +
+            "<td class='text-start'><input type='checkbox' class='form-check-input tbody_checkbox'></td>" +
+            "<td></td>" +
+            "<td></td>" +
+            "<td></td>" +
+            "<td class='phone_table_detail'></td>" +
+            "<td></td>" +
+            "<tr>"
         );
 
-        var button = createRemoveRowButton(newRow);
-        button.appendTo(newRow.children().get(4));
+        newRow.children().eq(2).text(surnameField.val());
+        newRow.children().eq(3).text(nameField.val());
+        newRow.children().eq(4).text("+7-" + phoneField.val());
+        newRow.children().eq(5).html(createRemoveRowButton(newRow));
 
-        var tbody = $("tbody");
+        var tbody = $(".phonebook_table_body");
         newRow.appendTo(tbody);
     }
 
@@ -78,14 +74,15 @@ $(document).ready(function () {
                 $("#canceledRemoveButton").click(function () {
                     content.text("");
                 });
+                $("#modal_window_close_button").click(function () {
+                    content.text("");
+                });
 
                 if (checkedRows.length > 0) {
                     content.text("Удалить отмеченные контакты?");
 
                     $("#confirmedRemoveButton").click(function () {
-                        $.each(checkedRows, function () {
-                            this.remove();
-                        });
+                        $(checkedRows).remove();
                         content.text("");
                         $("#checkAll").prop("checked", false);
                     });
@@ -94,9 +91,9 @@ $(document).ready(function () {
                     var name = row.children().get(2).innerHTML;
                     var phone = row.children().get(3).innerHTML;
 
-                    $("<p><b>" + "Фамилия:</b> " + surname + "</p>").appendTo(content);
-                    $("<p><b>" + "Имя:</b> " + name + "</p>").appendTo(content);
-                    $("<p><b>" + "Номер телефона:</b> " + phone + "</p>").appendTo(content);
+                    $("<p><b>Фамилия:</b> " + surname + "</p>").appendTo(content);
+                    $("<p><b>Имя:</b> " + name + "</p>").appendTo(content);
+                    $("<p><b>Номер телефона:</b> " + phone + "</p>").appendTo(content);
 
                     $("#confirmedRemoveButton").on("click", function () {
                         row.remove();
@@ -106,19 +103,14 @@ $(document).ready(function () {
             });
     }
 
-    function getPhoneArray() {
-        var phoneTableDetails = $(".phone_table_detail");
-
-        return $.map(phoneTableDetails, function (one) {
-            return one.textContent.substring(3);
+    function getPhonesArray() {
+        return $(".phone_table_detail").map(function () {
+            return this.textContent.substring(3);
         });
     }
 
     function getCheckedRows() {
-        var checkedRows = $(":checked");
-
-        return $.map(checkedRows, function (one) {
-            return one.closest(".tbody_row");
-        });
+        return $(".tbody_row :first-child :checked")
+            .closest(".tbody_row");
     }
 });
